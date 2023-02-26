f33689e433474245ff4e04897e730ebc9e4104fa
Crowdbotics <engineering@crowdbotics.com>
2023-02-26 01:43:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-wave-dev-66352",
+  url: "https://falling-wave-dev-66352.botics.co",
+  api: "https://falling-wave-dev-66352.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
